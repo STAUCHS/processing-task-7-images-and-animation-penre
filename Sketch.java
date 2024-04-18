1e6733999eccca10e6228b9af9daa84bacdd2b01
penre <tianyou.zhang25@ycdsbk12.ca>
2024-04-18 11:49:26 -0400

    resize image remember

--- Sketch.java
+++ Sketch.java
@@ -4,19 +4,25 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
 
   PImage imgWeirdDog;
+  PImage imgTrav;
 
-    // x and y coordinate for meteor
+    // x and y coordinate for dog
     float fltWeirdDogX = 50;
     float fltWeirdDogY = 100;
+
+    // x and y coordinate for cube
+    float fltCubeX = 50;
+    float fltCubeY = 100;
   
-    // meteor speed variables
-    float fltXSpeed = random(1, 5);
-    float fltYSpeed = random(1, 5);
+    // dog speed variables
+    float fltXSpeed = random(1, 100);
+    float fltYSpeed = random(1, 100);
+    //cube speed variables
+    float fltCubeXSpeed = random(1, 100);
+    float fltCuveYSpeed = random(1, 100);
   
     // define colours
-    int black = color(0);
-    int white = color(255);
-    int magenta = color(255, 0, 255);
+    int red = color(255,0,0);
   
     public void settings() {
       size(400, 400);
@@ -24,26 +30,25 @@ public class Sketch extends PApplet {
   
     public void setup() {
       background(0, 0, 0);
-      // load meteor
-      imgWeirdDog = loadImage("spaceMeteors_003.png");
-      // resize meteor
+      // load dog
+      imgWeirdDog = loadImage("main-qimg-45ff4af92effeedae4454bc970ac2dab-lq-removebg-preview.png");
+      // resize dog
       
-      imgWeirdDog.resize(50, 50);
+      imgWeirdDog.resize(80, 80);
       
-      // load missile
-  
-      // resize missile
-  
-      // load rocket
-  
-      // resize rocket
-  
+      
     }
+    
+    
+
+    
   
     public void draw() {
-      background(black);
+      //background image;
+      imgTrav = loadImage("tsfishcover.png");
+      imgTrav.resize
   
-      // draw meteor and move
+      // draw dog and move
       image(imgWeirdDog, fltWeirdDogX, fltWeirdDogY);
   
       fltWeirdDogX += fltXSpeed;
@@ -56,11 +61,10 @@ public class Sketch extends PApplet {
       if (fltWeirdDogY > height - imgWeirdDog.height || fltWeirdDogY < 0) {
         fltYSpeed *= -1;
       }
-  
-      // draw missile and move
-  
-  
-      // draw rocket and move
-  
+      //draw a square cube 
+      fill(red);
+      rect(100,100,50,50);
+      
+      
     }
   }
