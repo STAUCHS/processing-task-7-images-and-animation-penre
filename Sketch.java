b3b5df8f0037d88b33efe45192509bd837582643
penre <Tianyou.zhang25@ycdsbk12.ca>
2024-04-18 17:44:46 -0400

    finished

--- Sketch.java
+++ Sketch.java
@@ -4,45 +4,52 @@ import processing.core.PImage;
 public class Sketch extends PApplet {
 
   PImage imgWeirdDog;
-  PImage imgTrav;
+  PImage imgBack;
 
     // x and y coordinate for dog
-    float fltWeirdDogX = 50;
+    float fltWeirdDogX = 38;
     float fltWeirdDogY = 100;
 
     // x and y coordinate for cube
     float fltCubeX = 50;
     float fltCubeY = 100;
+    float fltCubeWidth = 50;
+    float fltCubeHeight = 50;
+    float fltAngle = 0;
+    float fltRadius = 100;
+    double dblAnglerSpeed = 0.05;
+
     // x and y coordinate for background
-    float fltTravX = 0;
-    float fltTravY = 0;
+    float fltBackX = 0;
+    float fltBackY = 0;
   
     // dog speed variables
-    float fltXSpeed = random(1, 100);
-    float fltYSpeed = random(1, 100);
-    //cube speed variables
-    float fltCubeXSpeed = random(1, 100);
-    float fltCuveYSpeed = random(1, 100);
+    float fltXSpeed = random(1, 5);
+    float fltYSpeed = random(1, 5);
   
-    // define colours
-    int red = color(255,0,0);
+    // define cube colours
+    int purple = color(128,0,128);
   
     public void settings() {
       size(400, 400);
     }
   
     public void setup() {
+
+      //clear out old frames
       background(0, 0, 0);
+
       // load dog
       imgWeirdDog = loadImage("main-qimg-45ff4af92effeedae4454bc970ac2dab-lq-removebg-preview.png");
+
       // resize dog
-      
       imgWeirdDog.resize(80, 80);
 
-      //load backgroudn
-      imgTrav = loadImage("tsfishcover.png");
-      //resize dog
-      imgTrav.resize(400,400);
+      //load background
+      imgBack = loadImage("251f49b9061e3ef0b3a862135258f151.png");
+
+      //resize background
+      imgBack.resize(400,400);
       
       
     }
@@ -54,7 +61,7 @@ public class Sketch extends PApplet {
     public void draw() {
       
       //background image;
-      image(imgTrav,fltTravX,fltTravY);
+      image(imgBack,fltBackX,fltBackY);
 
   
       // draw dog and move
@@ -70,10 +77,16 @@ public class Sketch extends PApplet {
       if (fltWeirdDogY > height - imgWeirdDog.height || fltWeirdDogY < 0) {
         fltYSpeed *= -1;
       }
-      //draw a square cube 
-      fill(red);
-      rect(100,100,50,50);
+     
+      //draw a square cube and move in a circle
+      fill(purple);
+      rect(fltCubeX, fltCubeY, fltCubeWidth, fltCubeHeight);
+
+      fltCubeX = width/2 + fltRadius * cos(fltAngle);
+      fltCubeY = height/2 + fltRadius * sin(fltAngle);
+      fltAngle += dblAnglerSpeed;
       
       
+      }
     }
-  }
+  
